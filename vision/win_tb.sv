416794a1f115a96d98e1539283a0d0cfa62082f8
Dylan Koenig <koenigup@gmail.com>
2013-12-01 21:34:22 -0500

    removed accRowTotal from outputs

--- vision/win_tb.sv
+++ vision/win_tb.sv
@@ -5,26 +5,36 @@ module tb();
 	bit [35:0] descIn;
 	bit signed [8:0] windowIn [15:0] [15:0];
 	bit signed [31:-32] greatestNCC;
-	bit [8:0] greatestWinIndex;
-	bit [31:0] accRowTotal [15:0];
+	bit [11:0] greatestWinIndex;
 	bit [31:0] num, denom;
 
 	ncc ncc_inst(clk, rst, window_data_ready, desc_data_ready, descIn, windowIn, 
-		done_with_window_data, done_with_desc_data, greatestNCC, greatestWinIndex, accRowTotal);
+		done_with_window_data, done_with_desc_data, greatestNCC, greatestWinIndex);
 
 	initial begin
 		clk = 0;
 		forever #5 clk = ~clk;
 	end
 	initial begin
-		$monitor($stime,,"********************************************************************************\nart1=%d, art15=%d, art16=%d, numTot=%d, winIndex=%d\ngreatestNCC=%b.%b\nnum=%b.%b\nden=%b.%b\ncorrCoeffLog2=%b.%b\ndsos=%d, wsos=%d", accRowTotal[0], accRowTotal[14], accRowTotal[15], ncc.accPatchSum, greatestWinIndex, greatestNCC[31:0], greatestNCC[-1:-32], ncc.numeratorLog2[9:0], ncc.numeratorLog2[-1:-54], ncc.denomLog2[9:0], ncc.denomLog2[-1:-54], ncc.corrCoeffLog2[9:0], ncc.corrCoeffLog2[-1:-54], ncc.descSumOfSquares, ncc.winSumOfSquares);
-		descIn[35:27] = 9'sd30;
-		descIn[26:18] = 9'sd40;
-		descIn[17:9] = 9'sd50;
-		descIn[8:0] = 9'sd60;
+		$monitor($stime,,"********************************************************************************\nart1=%d, art15=%d, art16=%d, numTot=%d, winIndex=%d\ngreatestNCC=%b.%b\nnum=%b.%b\nden=%b.%b\ncorrCoeffLog2=%b.%b\ndsos=%d, wsos=%d", ncc.accRowTotal[0], ncc.accRowTotal[14], ncc.accRowTotal[15], ncc.accPatchSum, greatestWinIndex, greatestNCC[31:0], greatestNCC[-1:-32], ncc.numeratorLog2[9:0], ncc.numeratorLog2[-1:-54], ncc.denomLog2[9:0], ncc.denomLog2[-1:-54], ncc.corrCoeffLog2[9:0], ncc.corrCoeffLog2[-1:-54], ncc.descSumOfSquares, ncc.winSumOfSquares);
+		descIn[35:27] = -9'sd1;
+		descIn[26:18] = 9'sd1;
+		descIn[17:9] = -9'sd1;
+		descIn[8:0] = 9'sd1;
 		for (int row = 0; row < 16; row++) begin
 			for (int col = 0; col < 16; col++) begin
-				windowIn[row][col] = 9'sd2;
+				if (col%4 == 0) begin
+					windowIn[row][col] = -9'sd1;
+				end
+				else if (col%4 == 1) begin
+					windowIn[row][col] = 9'sd1;
+				end
+				else if (col%4 == 2) begin
+					windowIn[row][col] = -9'sd1;
+				end
+				else if (col%4 == 3) begin
+					windowIn[row][col] = 9'sd1;
+				end
 			end
 		end
 		rst <= 1'b1;
