a3ea5b21815139d4ad31b610cd1a2cd064c75bc6
z1701Ez <sgun123@hotmail.com>
2013-12-04 01:41:37 -0500

    updated tree adder.

--- pipeline/tbTree_adder.sv
+++ pipeline/tbTree_adder.sv
@@ -4,9 +4,9 @@ module tree_adderTest;
 	logic signed [16:0] sum_result;
 	logic		clk;
 	logic		rst_n;
+	logic		enable;
 
-
-tree_adder #(9) dut(array, sum_result);
+tree_adder #(9) dut(clk,rst_n,enable,array, sum_result);
 int i,j;
 
 
@@ -24,13 +24,46 @@ end
 initial begin
 	for(i = 0; i <16 ; i++)begin
 		for(j = 0; j <16 ; j++)begin
-			array[i][j] = 9'sd1;
+			array[i][j] = -9'sb11111111;
 		end
 	end
 
-	#100;
 
-	$display($time, , "array = %d, sum = %d",array[0][0], sum_result);
+
+	#100;
+	enable = 1'b1;
+	@(posedge clk);
+	for(i = 0; i <16 ; i++)begin
+		for(j = 0; j <16 ; j++)begin
+			array[i][j] = 9'sd23;
+		end
+	end
+	@(posedge clk);
+	for(i = 0; i <16 ; i++)begin
+		for(j = 0; j <16 ; j++)begin
+			array[i][j] = 9'sd68;
+		end
+	end
+	@(posedge clk);
+	for(i = 0; i <16 ; i++)begin
+		for(j = 0; j <16 ; j++)begin
+			array[i][j] = -9'sd412;
+		end
+	end
+	@(posedge clk);
+	for(i = 0; i <16 ; i++)begin
+		for(j = 0; j <16 ; j++)begin
+			array[i][j] = -9'sd689;
+		end
+	end
+	@(posedge clk);
+	for(i = 0; i <16 ; i++)begin
+		for(j = 0; j <16 ; j++)begin
+			array[i][j] = 9'sd88;
+		end
+	end
+	repeat(100) @(posedge clk);
+	//$display($time, , "array = %d, sum = %d",array[0][0], sum_result);
 
 
 	$finish;
--- pipeline/tree_adder.sv
+++ pipeline/tree_adder.sv
@@ -1,19 +1,31 @@
 module tree_adder
 #(parameter inputSize = 9)
-(input logic signed [inputSize-1:0] operand[16][16] ,
+(input logic clk,
+input logic rst_n,
+input logic enable,
+input logic signed [inputSize-1:0] operand[16][16] ,
 output logic signed [inputSize-1+8:0] sum_result);
 
-logic signed [inputSize +1 -1:0] sum_0[128];
-logic signed [inputSize +2 -1:0] sum_1[64];
-logic signed [inputSize +3 -1:0] sum_2[32];
-logic signed [inputSize +4 -1:0] sum_3[16];
-logic signed [inputSize +5 -1:0] sum_4[8];
-logic signed [inputSize +6 -1:0] sum_5[4];
-logic signed [inputSize +7 -1:0] sum_6[2];
-logic signed [inputSize +8 -1:0] sum_7;
+logic signed [inputSize +1 -1:0] sum_0_in[128];
+logic signed [inputSize +2 -1:0] sum_1_in[64];
+logic signed [inputSize +3 -1:0] sum_2_in[32];
+logic signed [inputSize +4 -1:0] sum_3_in[16];
+logic signed [inputSize +5 -1:0] sum_4_in[8];
+logic signed [inputSize +6 -1:0] sum_5_in[4];
+logic signed [inputSize +7 -1:0] sum_6_in[2];
+logic signed [inputSize +8 -1:0] sum_7_in;
+
+logic signed [inputSize +1 -1:0] sum_0_out[128];
+logic signed [inputSize +2 -1:0] sum_1_out[64];
+logic signed [inputSize +3 -1:0] sum_2_out[32];
+logic signed [inputSize +4 -1:0] sum_3_out[16];
+logic signed [inputSize +5 -1:0] sum_4_out[8];
+logic signed [inputSize +6 -1:0] sum_5_out[4];
+logic signed [inputSize +7 -1:0] sum_6_out[2];
+logic signed [inputSize +8 -1:0] sum_7_out;
 
 
-assign sum_result = sum_7;
+assign sum_result = sum_7_out;
 //pair off all the values.
 // first pairing 256 ==> 128
 
@@ -22,7 +34,8 @@ generate
 
 	for(i = 0 ; i < 128; i++) begin
 	// 128 sum
-		adder#(inputSize) stage0(.input_A(operand[i/8][(i%8)*2]),.input_B(operand[i/8][(i%8)*2 +1]),.out(sum_0[i]));
+		adder#(inputSize) stage0(.input_A(operand[i/8][(i%8)*2]),.input_B(operand[i/8][(i%8)*2 +1]),.out(sum_0_in[i]));
+		adder_reg#(inputSize) re_0(.clk(clk),.rst_n(rst_n),.in(sum_0_in[i]),.enable(enable),.out(sum_0_out[i]));
 
 	end
 endgenerate
@@ -31,7 +44,10 @@ generate
 // second pairing 128 ==>64
 for(i = 0; i< 64; i++)begin
 
-adder#(inputSize+1) stage1(.input_A(sum_0[i*2]),.input_B(sum_0[i*2+1]),.out(sum_1[i]));
+adder#(inputSize+1) stage1(.input_A(sum_0_out[i*2]),.input_B(sum_0_out[i*2+1]),.out(sum_1_in[i]));
+adder_reg#(inputSize+1) re_1(.clk(clk),.rst_n(rst_n),.in(sum_1_in[i]),.enable(enable),.out(sum_1_out[i]));
+
+
 
 end
 endgenerate
@@ -40,7 +56,9 @@ generate
 // third pairing 64 ==>32
 for(i = 0; i<32; i++)begin
 
-adder#(inputSize+2) stage2(.input_A(sum_1[i*2]),.input_B(sum_1[i*2+1]),.out(sum_2[i]));
+adder#(inputSize+2) stage2(.input_A(sum_1_out[i*2]),.input_B(sum_1_out[i*2+1]),.out(sum_2_in[i]));
+adder_reg#(inputSize+2) re_2(.clk(clk),.rst_n(rst_n),.in(sum_2_in[i]),.enable(enable),.out(sum_2_out[i]));
+
 
 end
 endgenerate
@@ -49,7 +67,9 @@ generate
 // forth pairing 32 ==>16
 for( i = 0; i<16; i++)begin
 
-adder#(inputSize+3) stage3(.input_A(sum_2[i*2]),.input_B(sum_2[i*2+1]),.out(sum_3[i]));
+adder#(inputSize+3) stage3(.input_A(sum_2_out[i*2]),.input_B(sum_2_out[i*2+1]),.out(sum_3_in[i]));
+adder_reg#(inputSize+3) re_3(.clk(clk),.rst_n(rst_n),.in(sum_3_in[i]),.enable(enable),.out(sum_3_out[i]));
+
 
 end
 endgenerate
@@ -59,7 +79,9 @@ generate
 
 for( i = 0; i <8; i++)begin
 
-adder#(inputSize+4) stage4(.input_A(sum_3[i*2]),.input_B(sum_3[i*2+1]),.out(sum_4[i]));
+adder#(inputSize+4) stage4(.input_A(sum_3_out[i*2]),.input_B(sum_3_out[i*2+1]),.out(sum_4_in[i]));
+adder_reg#(inputSize+4) re_4(.clk(clk),.rst_n(rst_n),.in(sum_4_in[i]),.enable(enable),.out(sum_4_out[i]));
+
 
 end
 endgenerate
@@ -68,7 +90,9 @@ generate
 
 for( i = 0; i < 4; i++)begin
 
-adder#(inputSize+5) stage5(.input_A(sum_4[i*2]),.input_B(sum_4[i*2+1]),.out(sum_5[i]));
+adder#(inputSize+5) stage5(.input_A(sum_4_out[i*2]),.input_B(sum_4_out[i*2+1]),.out(sum_5_in[i]));
+adder_reg#(inputSize+5) re_5(.clk(clk),.rst_n(rst_n),.in(sum_5_in[i]),.enable(enable),.out(sum_5_out[i]));
+
 
 end
 endgenerate
@@ -77,7 +101,9 @@ generate
 
 for( i = 0; i <2; i++)begin
 
-adder#(inputSize+6) stage6(.input_A(sum_5[i*2]),.input_B(sum_5[i*2+1]),.out(sum_6[i]));
+adder#(inputSize+6) stage6(.input_A(sum_5_out[i*2]),.input_B(sum_5_out[i*2+1]),.out(sum_6_in[i]));
+adder_reg#(inputSize+6) re_6(.clk(clk),.rst_n(rst_n),.in(sum_6_in[i]),.enable(enable),.out(sum_6_out[i]));
+
 
 end
 endgenerate
@@ -85,7 +111,8 @@ endgenerate
 generate
 // eigth pairing 2 ==>1
 
-adder#(inputSize+7) stage7(.input_A(sum_6[0]),.input_B(sum_6[1]),.out(sum_7));
+adder#(inputSize+7) stage7(.input_A(sum_6_out[0]),.input_B(sum_6_out[1]),.out(sum_7_in));
+adder_reg#(inputSize+7) re_7(.clk(clk),.rst_n(rst_n),.in(sum_7_in),.enable(enable),.out(sum_7_out));
 
 endgenerate
 
@@ -101,3 +128,23 @@ assign out = input_A + input_B;
 
 endmodule: adder
 
+
+module adder_reg #(parameter inputSize = 9)
+		 (input logic clk,
+		  input logic rst_n,
+		  input logic signed [inputSize:0] in,
+		  input logic enable,
+		  output logic signed[inputSize:0] out);
+
+always_ff@(posedge clk,negedge rst_n)begin
+	if(~rst_n)begin
+		out <= 'd0;
+	end
+	else if (enable)begin
+		out <= in;
+	end
+	else begin
+		out <= out;
+	end
+end
+endmodule: adder_reg
